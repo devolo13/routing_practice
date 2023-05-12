355cc3368430a543b6056c90988ea4776c1434cd
devolo13 <devolo13@gmail.com>
2023-05-12 13:48:04 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+// import thing from ./place.jsx
+import{Routes, Route, Link, useParams, useSearchParams} from 'react-router-dom';
+
+const Home = (props) => {
+  return (
+    <h1 className='mt-3'>Welcome</h1>
+  );
+}
+
+const Test = (props) => {
+  const {word} = useParams();
+  console.log(word);
+  return (
+    <h1 mt-3>Test Component</h1>
+  );
+}
+
+const DisplayInfo = (props) => {
+  const {info} = useParams();
+  const {color1} = useParams();
+  const {color2} = useParams();
+  if (isNaN(info)){
+    if (color1 != undefined && color2 != undefined){
+      return(
+        <h1 className='mt-3' style={{color: color1, backgroundColor: color2}}>The word is: {info}</h1>
+      )
+    }
+    return(
+      <h1 className='mt-3'>The word is: {info}</h1>
+    );
+  }
+  return(
+    <h1 className='mt-3'>The number is: {info}</h1>
+  )
+}
+
+function App() {
+  return (
+    <div className="container">
+      {/* <h1 className="text-center">testing</h1> */}
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/:info' element={<DisplayInfo/>}/>
+        <Route path='/:info/:color1' element={<DisplayInfo/>}/>
+        <Route path='/:info/:color1/:color2' element={<DisplayInfo/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
